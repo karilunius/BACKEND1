fb3ed9e535837753f7c0e62180d7ddf3ca69b59f
karilunius <93058053+karilunius@users.noreply.github.com>
2022-10-27 13:43:01 -0500

    commit - mesa9 proh2

--- 009-practicoh2/src/src/Testlog.java
+++ 009-practicoh2/src/src/Testlog.java
@@ -1,4 +1,9 @@
-public class TestLog {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Testlog {
 
     public static void main(String[] args) throws Exception {
         Class.forName("org.h2.Driver").newInstance();
