a2b1ca558d027a8ca5d30bbe3545824088ff5bdf
karilunius <93058053+karilunius@users.noreply.github.com>
2022-10-27 13:18:02 -0500

    commit - mesa 8

--- 008-mesa8Actividad2Logueo/src/Main.java
+++ 008-mesa8Actividad2Logueo/src/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) throws Exception {
-        File log4jfile = new File("/Users/benjabarreiro/Documents/IdeaProjects/Actividad2Logueo/log4j.properties");
+        File log4jfile = new File("C:\\Users\\Karol\\Desktop\\BACKEND1\\008-mesa8Actividad2Logueo\\log4j.properties");
         PropertyConfigurator.configure(log4jfile.getAbsolutePath());
         List<Integer> miLista = List.of(3, 1, 4, 1,1,1,1,1,1,1,1,1);
         List<Integer> listaVacia = List.of();
--- 008-practicoClaseLogueo/src/Main.java
+++ 008-practicoClaseLogueo/src/Main.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) throws Exception {
-        File log4jfile = new File("/Users/benjabarreiro/Documents/IdeaProjects/ClaseLogueo2/log4j.properties.properties");
+        File log4jfile = new File("C:\\Users\\Karol\\Desktop\\BACKEND1\\008-practicoClaseLogueo\\log4j.properties");
         PropertyConfigurator.configure(log4jfile.getAbsolutePath());
 
         Leon leon = new Leon("Simba", -9, true);
