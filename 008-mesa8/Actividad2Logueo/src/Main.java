1c3caf2b0b7dfadafe215b00fae21bc2cd2c9a7f
karilunius <93058053+karilunius@users.noreply.github.com>
2022-10-27 12:39:53 -0500

    commit - mesa 8

---
+++ 008-mesa8/Actividad2Logueo/src/Main.java
@@ -0,0 +1,20 @@
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.io.File;
+import java.util.List;
+
+public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class);
+    public static void main(String[] args) throws Exception {
+        File log4jfile = new File("/Users/benjabarreiro/Documents/IdeaProjects/Actividad2Logueo/log4j.properties");
+        PropertyConfigurator.configure(log4jfile.getAbsolutePath());
+        List<Integer> miLista = List.of(3, 1, 4, 1,1,1,1,1,1,1,1,1);
+        List<Integer> listaVacia = List.of();
+        Lista lista = new Lista(miLista);
+        Lista lista2 = new Lista(listaVacia);
+        lista.validarLista();
+        lista2.validarLista();
+
+    }
+}
